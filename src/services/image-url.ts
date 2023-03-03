e54cdbab3e61c488b17065aec2deb23b4f1643d7
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-03-03 09:36:19 -0800

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,7 @@
+import noImage from '../assets/no-image-placeholder.webp';
 
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return '';
+  if (!url) return noImage;
   
   const target = 'media/';
   const index = url.indexOf(target) + target.length;
