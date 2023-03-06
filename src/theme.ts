a9f07464c6b8df3679624684cc504a42dae10bcc
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-03-06 11:42:37 -0800

    Customize the theme to get darker grays

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: 'dark'
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({ 
+  config,
+  colors: {
+    gray: {
+      50: '#f9f9f9',
+      100: '#ededed',
+      200: '#d3d3d3',
+      300: '#b3b3b3',
+      400: '#a0a0a0',
+      500: '#898989',
+      600: '#6c6c6c',
+      700: '#202020',
+      800: '#121212',
+      900: '#111'
+    }
+  }
+ });
 
 export default theme;
