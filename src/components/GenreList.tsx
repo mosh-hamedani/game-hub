a2028cfbe7458c6b0d570640a4e83fcdc4342218
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-03-06 13:34:22 -0800

    Change the genre font to md

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -41,7 +41,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
                 textAlign="left"
                 fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
-                fontSize="lg"
+                fontSize="md"
                 variant="link"
               >
                 {genre.name}
