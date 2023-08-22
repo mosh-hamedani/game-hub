3d5f7622c8ef623abe29afbea0e4abb416b57150
Ryan Biondo <biondoryan.dev@gmail.com>
2023-08-21 20:03:53 -0600

    Cloned repo and set up dependencies

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "9b32ff2a0d824d3595a410bc6da75c11",
   },
 });
