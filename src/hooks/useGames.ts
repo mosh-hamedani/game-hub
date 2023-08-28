29600bf794d2f6a65ac065b88f9064e01ef3de74
M-Huzaifa00 <121715060+M-Huzaifa00@users.noreply.github.com>
2023-08-28 14:08:25 -0700

    removed dependencies

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,7 +14,7 @@ export interface Game {
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
-  description_raw:string;
+
 }
 
 const useGames = (pageSize: number) => {
