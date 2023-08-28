409eb17a3b3af80e1c106d8fb5df779b9a6b7abb
M-Huzaifa00 <121715060+M-Huzaifa00@users.noreply.github.com>
2023-08-28 16:31:03 -0700

    update api

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,22 +1,21 @@
 import axios, { AxiosRequestConfig } from "axios";
+import { API } from './API'
 
 export interface FetchResponse<T> {
   count: number;
   next:string | null;
   results: T[];
 }
-
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-
 const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "3e9fa37ef569498da299de5a59e06105",
+    key: API.API_KEY,
   },
 });
 
