f2760a8b42a4bd64f460d4a64f2cb48749438f5a
M-Huzaifa00 <121715060+M-Huzaifa00@users.noreply.github.com>
2023-08-28 16:18:15 -0700

    app empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,8 @@
-import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
-import { useState } from "react";
-import GameGrid from "./components/GameGrid";
-import GameHeading from "./components/GameHeading";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
 
 function App() {
 
-  return (
-    <Grid
-      templateAreas={{
-        base: `"main"`,
-        lg: `"aside main"`,
-      }}
-      templateColumns={{
-        base: '1fr',
-        lg: '250px 1fr'
-      }}
-    >
-      <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
-          <GenreList/>
-        </GridItem>
-      </Show>
-      <GridItem area="main">
-        <Box paddingLeft={2}>
-          <GameHeading/>
-          <Flex marginBottom={5}>
-            <Box marginRight={5}>
-              <PlatformSelector/>
-            </Box>
-            <SortSelector/>
-          </Flex>
-        </Box>
-        <GameGrid/>
-      </GridItem>
-    </Grid>
-  );
+  // return (
+  // );
 }
 
 export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { HStack, Text, Box } from '@chakra-ui/react'
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
 import { Link } from 'react-router-dom';
+import '../App.css'
 
 const NavBar = () => {
   return (
