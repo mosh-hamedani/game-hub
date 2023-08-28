0887854aa9ee61ef2c839f933d724e1e9fd1f994
M-Huzaifa00 <121715060+M-Huzaifa00@users.noreply.github.com>
2023-08-28 14:30:33 -0700

    set up read moe or less logics

--- src/Pages/GameDetailPage.tsx
+++ src/Pages/GameDetailPage.tsx
@@ -1,16 +1,29 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react"
+import { Button, Heading, Spinner, Text } from "@chakra-ui/react"
 import { useParams } from 'react-router-dom'
 import { useGame } from "../hooks/useGame"
+import { useState } from 'react'
 
 export const GameDetailPage = () => {
     const { slug } = useParams()
-    const { data : game, isLoading , error} = useGame(slug!);
-    if(isLoading) return <Spinner/>;
-    if(error || !game) throw  error;
-  return (
-    <div>
-        <Heading>{game.name}</Heading>
-        <Text>{game.description_raw}</Text>
-    </div>
-  )
+    const { data: game, isLoading, error } = useGame(slug!)
+    if (isLoading) return <Spinner />;
+    if (error || !game) throw error;
+    const [desc, setDesc] = useState(game.description_raw.substring(0, 299) + '...')
+
+    return (
+        <div>
+            <Heading>{game.name}</Heading>
+            {
+                game.description_raw.length < 300 ? <Text>{game.description_raw}</Text> :
+                    <Text>{desc}
+                        <Button
+                            onClick={() => {
+                                desc.length < 300 ? setDesc(game.description_raw) : game.description_raw.substring(0, 299) + '...';
+                            }}
+                        >Read more</Button>
+                    </Text>
+            }
+
+        </div>
+    )
 }
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -14,6 +14,7 @@ export interface Game {
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
+  description_raw:string;
 
 }
 
